036a1331da1f13761897078917c581e8438df474
Chyngyz Isakov <ellegantus@gmail.com>
2023-05-17 17:37:12 +0600

    create changeNumbers and use functions

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,32 @@ const App = () => {
     let [number, setNumber] = useState([0, 0, 0, 0, 0]);
 
 
+    const getRandomBall = () => {
+        let randomBall: number[] = [];
+
+        let i = 0;
+
+        do {
+            let count = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+
+            if (!randomBall.includes(count)) {
+                randomBall.push(count);
+                i = i + 1;
+            }
+        } while (i < 5);
+        randomBall.sort((a, b) => a - b);
+        return randomBall
+    };
+    const changeNumbers = () => {
+        getRandomBall();
+        setNumber(getRandomBall);
+    };
+
 
 
     return (
         <div className="App">
-            <button className="change-btn" >New Numbers</button>
+            <button className="change-btn" onClick={changeNumbers}>New Numbers</button>
             <div>
                 <Game num={number[0]}/>
                 <Game num={number[1]}/>
